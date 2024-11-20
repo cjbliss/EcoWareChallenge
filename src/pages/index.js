611d7184eb023015fc50d68d4f644842688dfc85
Christian Bliss <123503814+cjbliss@users.noreply.github.com>
2024-11-20 14:22:36 -0500

    fixed search menu close

--- src/pages/index.js
+++ src/pages/index.js
@@ -157,7 +157,7 @@ export default function Home() {
       <SortMenu
           anchorEl={anchorEl}
           onMenuClick={handleMenuClick}
-          onClose={handleClose}
+          onClose={handleMenuClose}
           onSort={handleSort}
           sortOrder={sortOrder}
           />
